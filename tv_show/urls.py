e1ec420022d6720e254e2ab08e81c19776236bcf
MurMurichOG <muratmuhutdinov1122@gmail.com>
2024-02-04 14:17:35 +0600

    hw5(redone)

--- tv_show/urls.py
+++ tv_show/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import movies_list, movies_detail, MovieListView, search_movies
+from .views import MovieListView, search_movies, movie_detail, create_movie, update_movie, delete_movie, create_review
 
 urlpatterns = [
-    path('movies/', movies_list),
-    path('movies/<int:id>', movies_detail),
     path('movies/', MovieListView.as_view(), name='movie_list'),
-    path('search/', search_movies, name='search_movies')
+    path('search/', search_movies, name='search_movies'),
+    path('movies/<int:movie_id>/', movie_detail, name='movie_detail'),
+    path('movies/create/', create_movie, name='create_movie'),
+    path('movies/<int:movie_id>/update/', update_movie, name='update_movie'),
+    path('movies/<int:movie_id>/delete/', delete_movie, name='delete_movie'),
+    path('movies/<int:movie_id>/review/create/', create_review, name='create_review'),
 ]
--- tv_show/views.py
+++ tv_show/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Movie, Review
 from .models import TVShow
 from django.http import HttpResponseRedirect
@@ -22,6 +22,53 @@ def search_movies(request):
     movies = Movie.objects.filter(title__icontains=query)
     return render(request, 'search_results.html', {'movies': movies, 'query': query})
 
+def movie_detail(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    reviews = Review.objects.filter(movie=movie)
+    return render(request, 'movie_detail.html', {'movie': movie, 'reviews': reviews})
+
+def create_movie(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        Movie.objects.create(title=title, description=description)
+        return redirect('movie_list')
+    return render(request, 'create_movie.html')
+
+def update_movie(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    if request.method == 'POST':
+        movie.title = request.POST.get('title')
+        movie.description = request.POST.get('description')
+        movie.save()
+        return redirect('movie_list')
+    return render(request, 'update_movie.html', {'movie': movie})
+
+def delete_movie(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    if request.method == 'POST':
+        movie.delete()
+        return redirect('movie_list')
+    return render(request, 'delete_movie.html', {'movie': movie})
+
+def create_review(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+    if request.method == 'POST':
+        text = request.POST.get('text')
+        rating = request.POST.get('rating')
+        Review.objects.create(movie=movie, text=text, rating=rating)
+        return redirect('movie_list')
+    return render(request, 'create_review.html', {'movie': movie})
+
+def search_movies(request):
+    query = request.GET.get('q', '')
+    movies = Movie.objects.filter(title__icontains=query)
+    return render(request, 'search_results.html', {'movies': movies, 'query': query})
+
+def movies_list(request):
+    movies = Movie.objects.all()
+    return render(request, 'movies/movies.html',{'movies': movies})
+
 def show_list(request):
     shows = [...]
     return render(request, 'show_list.html', {'shows': shows})
@@ -44,10 +91,6 @@ def tv_show_detail(request, pk):
 def tv_show_list(request):
     tv_shows = TVShow.objects.all()
     return render(request, 'tv_show_list.html', {'tv_shows': tv_shows})
-
-def tv_show_detail(request, pk):
-    tv_show = get_object_or_404(TVShow, pk=pk)
-    return render(request, 'tv_show_detail.html', {'tv_show': tv_show})
 
 def tv_show_create(request):
     if request.method == 'POST':
@@ -77,11 +120,3 @@ def tv_show_delete(request, pk):
     tv_show = get_object_or_404(TVShow, pk=pk)
     tv_show.delete()
     return HttpResponseRedirect(reverse('tv_show_list'))
-
-def movies_list(request):
-    movies = Movie.objects.all()
-    return render(request, 'movies/movies.html',{'movies': movies})
-
-def movies_detail(request, id):
-    movie = get_object_or_404(Movie, id=id)
-    return render(request, 'movies/movies_detail.html', {'movie': movie})
