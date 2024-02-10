b6e7693dc44b3f2bfeef10fdfc9cc80171b04e7e
MurMurichOG <muratmuhutdinov1122@gmail.com>
2024-02-10 14:25:53 +0600

    hw7 (redone)

--- register/urls.py
+++ register/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import register
+from .views import register, user_list
 
 urlpatterns = [
     path('register/', register, name='register'),
+    path('user-list/', user_list, name='user_list'),
 ]
+
--- register/views.py
+++ register/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
+from .models import CustomUser
 
 def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login')
+            return redirect('user_list')
     else:
         form = CustomUserCreationForm()
 
     return render(request, 'registration/register.html', {'form': form})
 
+def user_list(request):
+    users = CustomUser.objects.all()
+    return render(request, 'myapp/user_list.html', {'users': users})
